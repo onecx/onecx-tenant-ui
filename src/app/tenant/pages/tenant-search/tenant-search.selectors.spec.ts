e940624c7bc92424c18ecdbb120cdf7a2ac556d7
Jannik Steenken <jannik.steenken@capgemini.com>
2024-04-02 21:04:11 +0200

    feat: selector test

--- src/app/tenant/pages/tenant-search/tenant-search.selectors.spec.ts
+++ src/app/tenant/pages/tenant-search/tenant-search.selectors.spec.ts
@@ -17,87 +17,92 @@ import {
       });
     });
   
-    // describe('selectTenantSearchViewModel', () => {
-    //   it('should combine the input to be the viewmodel', () => {
-    //     let columns : DataTableColumn[]= [{
-    //         columnType: ColumnType.STRING,
-    //         id: 'id',
-    //         nameKey: 'TENANT_SEARCH.COLUMNS.ID',
-    //         filterable: true,
-    //         sortable: true,
-    //         predefinedGroupKeys: [
-    //           'TENANT_SEARCH.PREDEFINED_GROUP.DEFAULT',
-    //           'TENANT_SEARCH.PREDEFINED_GROUP.EXTENDED',
-    //           'TENANT_SEARCH.PREDEFINED_GROUP.FULL',
-    //         ],
-    //       },
-    //       {
-    //         columnType: ColumnType.STRING,
-    //         id: 'orgId',
-    //         nameKey: 'TENANT_SEARCH.COLUMNS.ORG_ID',
-    //         filterable: true,
-    //         sortable: true,
-    //         predefinedGroupKeys: [
-    //           'TENANT_SEARCH.TENANT_GROUP.DEFAULT',
-    //           'TENANT_SEARCH.TENANT_GROUP.EXTENDED',
-    //           'TENANT_SEARCH.TENANT_GROUP.FULL',
-    //         ],
-    //       }
-    //     ];
+    describe('selectTenantSearchViewModel', () => {
+      it('should combine the input to be the viewmodel', () => {
+        let columns : DataTableColumn[]= [{
+            columnType: ColumnType.STRING,
+            id: 'id',
+            nameKey: 'TENANT_SEARCH.COLUMNS.ID',
+            filterable: true,
+            sortable: true,
+            predefinedGroupKeys: [
+              'TENANT_SEARCH.PREDEFINED_GROUP.DEFAULT',
+              'TENANT_SEARCH.PREDEFINED_GROUP.EXTENDED',
+              'TENANT_SEARCH.PREDEFINED_GROUP.FULL',
+            ],
+          },
+          {
+            columnType: ColumnType.STRING,
+            id: 'orgId',
+            nameKey: 'TENANT_SEARCH.COLUMNS.ORG_ID',
+            filterable: true,
+            sortable: true,
+            predefinedGroupKeys: [
+              'TENANT_SEARCH.TENANT_GROUP.DEFAULT',
+              'TENANT_SEARCH.TENANT_GROUP.EXTENDED',
+              'TENANT_SEARCH.TENANT_GROUP.FULL',
+            ],
+          }
+        ];
 
-    //     let searchCriteria = {
-    //         orgId: "1",
+        let searchCriteria = {
+            orgId: "1",
             
-    //     };
-    //     let results : RowListGridData[]= [{id: 1, imagePath: ''}];
-    //     let searchConfigs: SearchConfigInfo[] = [{id: '1', name: "test1"}];
-    //     let selectedSearchConfig = null;
-    //     let displayedColumns : DataTableColumn[]= [{
-    //         columnType: ColumnType.STRING,
-    //         id: 'id',
-    //         nameKey: 'TENANT_SEARCH.COLUMNS.ID',
-    //         filterable: true,
-    //         sortable: true,
-    //         predefinedGroupKeys: [
-    //           'TENANT_SEARCH.PREDEFINED_GROUP.DEFAULT',
-    //           'TENANT_SEARCH.PREDEFINED_GROUP.EXTENDED',
-    //           'TENANT_SEARCH.PREDEFINED_GROUP.FULL',
-    //         ],
-    //       },
-    //       {
-    //         columnType: ColumnType.STRING,
-    //         id: 'orgId',
-    //         nameKey: 'TENANT_SEARCH.COLUMNS.ORG_ID',
-    //         filterable: true,
-    //         sortable: true,
-    //         predefinedGroupKeys: [
-    //           'TENANT_SEARCH.TENANT_GROUP.DEFAULT',
-    //           'TENANT_SEARCH.TENANT_GROUP.EXTENDED',
-    //           'TENANT_SEARCH.TENANT_GROUP.FULL',
-    //         ],
-    //       }
-    //     ];
-    //     let viewMode = ;
-    //     let chartVisible = false;
-    //     let searchConfigEnabled = true;
-    //     expect(
-    //         selectTenantSearchViewModel.projector(
-    //             columns,
-    //             searchCriteria,
-    //             results,
-    //             searchConfigs,
-    //             selectedSearchConfig,
-    //             displayedColumns,
-    //             viewMode,
-    //             chartVisible,
-    //             searchConfigEnabled
-    //       )
-    //     ).toEqual({
-    //       count: buttonClickCount,
-    //       search: searchString,
-    //       results: searchResults,
-    //       resultsCount: searchResultsCount,
-    //     });
-    //   });
-    // });
+        };
+        let results : RowListGridData[]= [{id: 1, imagePath: ''}];
+        let searchConfigs: SearchConfigInfo[] = [{id: '1', name: "test1"}];
+        let selectedSearchConfig = null;
+        let displayedColumns : DataTableColumn[]= [{
+            columnType: ColumnType.STRING,
+            id: 'id',
+            nameKey: 'TENANT_SEARCH.COLUMNS.ID',
+            filterable: true,
+            sortable: true,
+            predefinedGroupKeys: [
+              'TENANT_SEARCH.PREDEFINED_GROUP.DEFAULT',
+              'TENANT_SEARCH.PREDEFINED_GROUP.EXTENDED',
+              'TENANT_SEARCH.PREDEFINED_GROUP.FULL',
+            ],
+          },
+          {
+            columnType: ColumnType.STRING,
+            id: 'orgId',
+            nameKey: 'TENANT_SEARCH.COLUMNS.ORG_ID',
+            filterable: true,
+            sortable: true,
+            predefinedGroupKeys: [
+              'TENANT_SEARCH.TENANT_GROUP.DEFAULT',
+              'TENANT_SEARCH.TENANT_GROUP.EXTENDED',
+              'TENANT_SEARCH.TENANT_GROUP.FULL',
+            ],
+          }
+        ];
+        let viewMode = 'advanced' as "basic" | "advanced";
+        let chartVisible = false;
+        let searchConfigEnabled = true;
+        expect(
+            selectTenantSearchViewModel.projector(
+                columns,
+                searchCriteria,
+                results,
+                searchConfigs,
+                selectedSearchConfig,
+                displayedColumns,
+                viewMode,
+                chartVisible,
+                searchConfigEnabled
+          )
+        ).toEqual({
+                columns: columns,
+                searchCriteria:searchCriteria,
+                results:results,
+                searchConfigs:searchConfigs,
+                selectedSearchConfig:selectedSearchConfig,
+                displayedColumns:displayedColumns,
+                viewMode:viewMode,
+                chartVisible:chartVisible,
+                searchConfigEnabled:searchConfigEnabled
+        });
+      });
+    });
   });
