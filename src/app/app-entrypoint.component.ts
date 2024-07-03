8302d2584ed2414657bea459a591ef3fb7e077de
markuczy <martin.kuczynski@capgemini.com>
2024-07-03 10:20:13 +0200

    fix: removed test and console log

--- src/app/app-entrypoint.component.ts
+++ src/app/app-entrypoint.component.ts
@@ -4,8 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app-entrypoint.component.html',
 })
-export class AppEntrypointComponent {
-  constructor() {
-    console.log('Entrypoint component was rendered');
-  }
-}
+export class AppEntrypointComponent {}
