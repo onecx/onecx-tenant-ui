97134fd6abd6cc7ae87dbd4072cd38f5b975726b
markuczy <martin.kuczynski@capgemini.com>
2024-07-03 10:14:17 +0200

    fix: app entrypoint test

--- src/app/app-entrypoint.component.spec.ts
+++ src/app/app-entrypoint.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppEntrypointComponent } from './app-entrypoint.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppEntrypointComponent],
-      imports: [],
+      imports: [RouterTestingModule],
       providers: [],
     }).compileComponents();
   });
