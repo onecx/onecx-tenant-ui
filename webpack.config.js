ecd021e0dc78a6022f25c2911d281cae238f4791
markuczy <martin.kuczynski@capgemini.com>
2024-07-18 10:54:37 +0200

    feat: shared all package-lock packages and removed singletons

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const { ModifyEntryPlugin } = require('@angular-architects/module-federation/src/utils/modify-entry-plugin')
+const {
+  ModifyEntryPlugin,
+} = require('@angular-architects/module-federation/src/utils/modify-entry-plugin');
 const {
   share,
   withModuleFederationPlugin,
@@ -11,29 +13,24 @@ const config = withModuleFederationPlugin({
   },
   shared: share({
     '@angular/core': {
-      singleton: true,
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
     '@angular/forms': {
-      singleton: true,
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
     '@angular/common': {
-      singleton: true,
       requiredVersion: 'auto',
       includeSecondaries: {
         skip: ['@angular/common/http/testing'],
       },
     },
     '@angular/common/http': {
-      singleton: true,
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
     '@angular/router': {
-      singleton: true,
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
@@ -41,16 +38,17 @@ const config = withModuleFederationPlugin({
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
-    '@ngx-translate/core': { singleton: true, requiredVersion: 'auto' },
+    '@ngx-translate/core': { requiredVersion: 'auto' },
     '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/angular-auth': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/angular-integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/angular-webcomponents': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/angular-integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/keycloak-auth': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/nx-plugin': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/portal-layout-styles': { requiredVersion: 'auto', includeSecondaries: true },
+    primeng: { requiredVersion: 'auto', includeSecondaries: true }
   }),
 
   sharedMappings: [],
@@ -58,21 +56,23 @@ const config = withModuleFederationPlugin({
 
 module.exports = config;
 
-const plugins = config.plugins.filter((plugin) => !(plugin instanceof ModifyEntryPlugin))
+const plugins = config.plugins.filter(
+  (plugin) => !(plugin instanceof ModifyEntryPlugin)
+);
 
 module.exports = {
   ...config,
   plugins,
   output: {
     uniqueName: 'onecx-tenant-ui',
-    publicPath: 'auto'
+    publicPath: 'auto',
   },
   experiments: {
     ...config.experiments,
-    topLevelAwait: true
+    topLevelAwait: true,
   },
   optimization: {
     runtimeChunk: false,
-    splitChunks: false
-  }
-}
+    splitChunks: false,
+  },
+};
