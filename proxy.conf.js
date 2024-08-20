ecaa534aca317d7ed8f5e80abaee8315c266251d
Christian Badura <christian.badura@capgemini.com>
2024-08-20 14:29:07 +0200

    fix: proxy conf and buildTarget in project json

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,84 +1,18 @@
-const bypassFn = function (req, res, proxyOptions) {
-  try {
-    if (req.method === 'OPTIONS') {
-      res.setHeader('Allow', 'GET, POST, HEAD, PUT, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', '*');
-      res.setHeader('Access-Control-Allow-Headers', '*');
-      return res.send('');
-    } else {
-      console.log('############## REQ ', req.url);
-      if (req.url === '/bff/searchConfig/infos/tenant-search') {
-        console.log('new search config created');
-        const searchConfigMock = {
-          totalElements: 3,
-          configs: [],
-        };
-
-        res.end(
-          JSON.stringify({
-            totalElements: searchConfigMock.totalElements,
-            configs: searchConfigMock.configs,
-          }),
-        );
-      } else if (req.url === '/bff/searchConfig/') {
-        const searchConfigMockCreated = {
-          totalElements: 4,
-          configs: [],
-        };
-
-        res.end(
-          JSON.stringify({
-            totalElements: searchConfigMockCreated.totalElements,
-            configs: searchConfigMockCreated.configs,
-          }),
-        );
-      } else if (
-        req.url === '/bff/searchConfig/2' ||
-        req.url === '/bff/searchConfig/1'
-      ) {
-        console.log('get search config with id');
-        const searchConfigSingleMock = {
-          config: {
-            id: 1,
-            page: 'tenant',
-            name: 'test',
-            modificationCount: 0,
-            fieldListVersion: 0,
-            isReadonly: false,
-            isAdvanced: false,
-            columns: [],
-            values: {
-              id: '10',
-            },
-          },
-        };
-
-        res.end(
-          JSON.stringify({
-            config: searchConfigSingleMock.config,
-          }),
-        );
-      } else if (req.url === '/bff/searchConfig/') {
-        console.log('searchConfig created');
-
-        const searchConfigMockCreated = {
-          totalElements: 4,
-          configs: [],
-        };
-
-        res.end(
-          JSON.stringify({
-            totalElements: searchConfigMockCreated.totalElements,
-            configs: searchConfigMockCreated.configs,
-          }),
-        );
-      } else {
-        return null;
-      }
-    }
-  } catch (error) {
-    console.log('error', error);
+/**
+ * Used in local environment:
+ *   Request proxy to rewrite URLs and prevent CORS errors
+ */
+const logFn = function (req, res) {
+  //console.log(new Date().toISOString() + `: bypassing ${req.method} ${req.url} `)
+};
+const onProxyRes = function (proxyRes, req, res) {
+  logFn(req, res);
+  if (req.method.toUpperCase() === 'OPTIONS') {
+    res.setHeader('Allow', 'GET, POST, HEAD, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    return res.send('');
   }
 };
 
@@ -91,7 +25,7 @@ const PROXY_CONFIG = {
     },
     changeOrigin: true,
     logLevel: 'debug',
-    bypass: bypassFn,
+    onProxyRes: onProxyRes,
   },
 };
 
