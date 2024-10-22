307e153fa4e2115a5cf1925e55466a1e58091f69
Henry Taeschner <henry.taeschner@capgemini.com>
2024-10-22 16:30:25 +0200

    fix: added platform-browser to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,68 +7,26 @@ const config = withModuleFederationPlugin({
     './OneCXTenantModule': './src/main.ts'
   },
   shared: share({
-    '@angular/core': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@angular/forms': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@angular/common': {
-      requiredVersion: 'auto',
-      includeSecondaries: {
-        skip: ['@angular/common/http/testing']
-      }
-    },
-    '@angular/common/http': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@angular/router': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    rxjs: {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
+    '@angular/core': { requiredVersion: 'auto', includeSecondaries: true },
+    '@angular/forms': { requiredVersion: 'auto', includeSecondaries: true },
+    '@angular/common': { requiredVersion: 'auto', includeSecondaries: { skip: ['@angular/common/http/testing'] } },
+    '@angular/common/http': { requiredVersion: 'auto', includeSecondaries: true },
+    '@angular/platform-browser': { requiredVersion: 'auto', includeSecondaries: true },
+    '@angular/router': { requiredVersion: 'auto', includeSecondaries: true },
     '@ngx-translate/core': { requiredVersion: 'auto' },
+    primeng: { requiredVersion: 'auto', includeSecondaries: true },
+    rxjs: { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/ngrx-accelerator': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/angular-auth': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@onecx/angular-webcomponents': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@onecx/integration-interface': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@onecx/angular-integration-interface': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@onecx/keycloak-auth': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
+    '@onecx/angular-auth': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/angular-webcomponents': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/angular-integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/keycloak-auth': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/nx-plugin': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/portal-integration-angular': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@onecx/portal-layout-styles': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
-    '@onecx/angular-accelerator': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    }
+    '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/portal-layout-styles': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/angular-accelerator': { requiredVersion: 'auto', includeSecondaries: true }
   }),
 
   sharedMappings: []
@@ -81,16 +39,7 @@ const plugins = config.plugins.filter((plugin) => !(plugin instanceof ModifyEntr
 module.exports = {
   ...config,
   plugins,
-  output: {
-    uniqueName: 'onecx-tenant-ui',
-    publicPath: 'auto'
-  },
-  experiments: {
-    ...config.experiments,
-    topLevelAwait: true
-  },
-  optimization: {
-    runtimeChunk: false,
-    splitChunks: false
-  }
+  output: { uniqueName: 'onecx-tenant-ui', publicPath: 'auto' },
+  experiments: { ...config.experiments, topLevelAwait: true },
+  optimization: { runtimeChunk: false, splitChunks: false }
 }
