a692fcbafc1268a80cf03c36be5ba0914f4bfb8c
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-06-24 14:21:56 +0200

    feat: full test coverage

--- src/app/tenant/pages/tenant-search/tenant-search.component.spec.ts
+++ src/app/tenant/pages/tenant-search/tenant-search.component.spec.ts
@@ -1,3 +1,4 @@
+import 'zone.js/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { provideHttpClient } from '@angular/common/http'
 import { provideHttpClientTesting } from '@angular/common/http/testing'
@@ -10,17 +11,26 @@ import { MockStore, provideMockStore } from '@ngrx/store/testing'
 import { TranslateTestingModule } from 'ngx-translate-testing'
 import { DialogService } from 'primeng/dynamicdialog'
 
-import { PortalCoreModule } from '@onecx/portal-integration-angular'
+import { ColumnType, DataTableColumn, PortalCoreModule, UserService } from '@onecx/portal-integration-angular'
 
 import { TenantSearchActions } from './tenant-search.actions'
 import { TenantSearchComponent } from './tenant-search.component'
 import { initialState } from './tenant-search.reducers'
+import { selectTenantSearchViewModel } from './tenant-search.selectors'
+import { TenantSearchViewModel } from './tenant-search.viewmodel'
+import { TenantSearchHarness } from './tenant-search.harness'
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed'
+import { TranslateService } from '@ngx-translate/core'
+import { of } from 'rxjs'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { tenantSearchCriteriasSchema } from './tenant-search.parameters'
 
 describe('TenantSearchComponent', () => {
   let component: TenantSearchComponent
   let fixture: ComponentFixture<TenantSearchComponent>
   let store: MockStore<Store>
   let formBuilder: FormBuilder
+  let tenantSearch: TenantSearchHarness
 
   const mockActivatedRoute = {}
 
@@ -52,7 +62,8 @@ describe('TenantSearchComponent', () => {
         TranslateTestingModule.withTranslations('en', require('./../../../../assets/i18n/en.json')).withTranslations(
           'de',
           require('./../../../../assets/i18n/de.json')
-        )
+        ),
+        NoopAnimationsModule
       ],
       providers: [
         DialogService,
@@ -68,14 +79,19 @@ describe('TenantSearchComponent', () => {
   })
   /* eslint-disable @typescript-eslint/no-var-requires */
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    const userService = TestBed.inject(UserService)
+    userService.hasPermission = () => true
+    const translateService = TestBed.inject(TranslateService)
+    translateService.use('en')
     fixture = TestBed.createComponent(TenantSearchComponent)
     component = fixture.componentInstance
     store = TestBed.inject(MockStore)
     formBuilder = TestBed.inject(FormBuilder)
     fixture.detectChanges()
+    tenantSearch = await TestbedHarnessEnvironment.harnessForFixture(fixture, TenantSearchHarness)
+    window.URL.createObjectURL = jest.fn()
   })
-
   it('should create the component', () => {
     expect(component).toBeTruthy()
   })
@@ -105,5 +121,199 @@ describe('TenantSearchComponent', () => {
     })
 
     component.onResetSearchCriteria()
+  })
+
+  it('should dispatch searchButtonClicked action on search', (done) => {
+    const date = new Date()
+    const formValue = formBuilder.group({
+      date: date
+    })
+    component.tenantSearchForm = formValue
+    component.visibleFormControls = [
+      {
+        name: 'changeMe'
+      }
+    ] as any
+
+    store.scannedActions$.pipe(ofType(TenantSearchActions.searchButtonClicked)).subscribe((a) => {
+      expect(a.searchCriteria).toEqual({ date: null })
+    })
+    done()
+
+    component.onSearch(formValue)
+  })
+
+  it('should convert valid date to UTC and falsy value to null in searchCriteria', (done) => {
+    const date = new Date(2023, 5, 15, 10, 30, 0)
+    const formValue = formBuilder.group({
+      validDate: date,
+      emptyField: '',
+      pageSize: '',
+      pageNumber: undefined
+    })
+
+    component.tenantSearchForm = formValue
+    component.visibleFormControls = [{ name: 'validDate' }, { name: 'emptyField' }] as any
+
+    store.scannedActions$.pipe(ofType(TenantSearchActions.searchButtonClicked)).subscribe((a) => {
+      expect(a.searchCriteria).toEqual({
+        validDate: new Date(Date.UTC(2023, 5, 15, 10, 30, 0)),
+        emptyField: null,
+        pageSize: null,
+        pageNumber: null
+      })
+    })
+    done()
+
+    component.onSearch(formValue)
+  })
+
+  it('should transform empty string to undefined', () => {
+    const parsed = tenantSearchCriteriasSchema.parse({
+      pageNumber: '',
+      pageSize: ''
+    })
+
+    expect(parsed.pageNumber).toBeUndefined()
+    expect(parsed.pageSize).toBeUndefined()
+  })
+
+  it('should dispatch view mode change', async () => {
+    jest.spyOn(store, 'dispatch')
+    const baseTenantSearchViewModel: TenantSearchViewModel = {
+      chartVisible: true,
+      columns: [{ columnType: ColumnType.STRING, id: '1', nameKey: 'orgId' }],
+      displayedColumns: [{ columnType: ColumnType.STRING, id: '1', nameKey: 'orgId' }],
+      results: [{ id: '1', imagePath: ' ' }],
+      searchCriteria: { orgId: '1' },
+      viewMode: 'basic'
+    }
+    store.overrideSelector(selectTenantSearchViewModel, {
+      ...baseTenantSearchViewModel,
+      chartVisible: false,
+      viewMode: 'advanced'
+    })
+    store.refreshState()
+
+    expect(store.dispatch).toHaveBeenCalledWith(TenantSearchActions.viewModeChanged({ viewMode: 'advanced' }))
+  })
+
+  it('should dispatch chart visibility change', async () => {
+    jest.spyOn(store, 'dispatch')
+    const baseTenantSearchViewModel: TenantSearchViewModel = {
+      chartVisible: true,
+      columns: [{ columnType: ColumnType.STRING, id: '1', nameKey: 'orgId' }],
+      displayedColumns: [{ columnType: ColumnType.STRING, id: '1', nameKey: 'orgId' }],
+      results: [{ id: '1', imagePath: ' ' }],
+      searchCriteria: { orgId: '1' },
+      viewMode: 'advanced'
+    }
+    store.overrideSelector(selectTenantSearchViewModel, {
+      ...baseTenantSearchViewModel,
+      chartVisible: false
+    })
+
+    store.refreshState()
+    const searchHeader = await tenantSearch.getHeader()
+    const pageHeader = await searchHeader.getPageHeader()
+    const overflowActionButton = await pageHeader.getOverflowActionMenuButton()
+    expect(overflowActionButton).toBeDefined()
+    await overflowActionButton?.click()
+    const showHideChartActionItem = await pageHeader.getOverFlowMenuItem('Show chart')
+    await showHideChartActionItem?.selectItem()
+    expect(store.dispatch).toHaveBeenCalledWith(TenantSearchActions.chartVisibilityToggled())
+
+    //hide again
+    store.overrideSelector(selectTenantSearchViewModel, {
+      ...baseTenantSearchViewModel,
+      chartVisible: true
+    })
+
+    store.refreshState()
+    const searchHeading = await tenantSearch.getHeader()
+    const pageHeading = await searchHeading.getPageHeader()
+    const overflowButton = await pageHeading.getOverflowActionMenuButton()
+    expect(overflowButton).toBeDefined()
+    await overflowButton?.click()
+    const hideChartActionItem = await pageHeading.getOverFlowMenuItem('Show chart')
+    await hideChartActionItem?.selectItem()
+    expect(store.dispatch).toHaveBeenCalledWith(TenantSearchActions.chartVisibilityToggled())
+  })
+
+  it('should dispatch export', async () => {
+    jest.spyOn(store, 'dispatch')
+    jest.spyOn(component, 'onExportItems')
+
+    const baseTenantSearchViewModel: TenantSearchViewModel = {
+      chartVisible: true,
+      columns: [{ columnType: ColumnType.STRING, id: '1', nameKey: 'orgId' }],
+      displayedColumns: [{ columnType: ColumnType.STRING, id: '1', nameKey: 'orgId' }],
+      results: [{ id: '1', imagePath: ' ' }],
+      searchCriteria: { orgId: '1' },
+      viewMode: 'advanced'
+    }
+    store.overrideSelector(selectTenantSearchViewModel, {
+      ...baseTenantSearchViewModel,
+      chartVisible: false
+    })
+
+    store.refreshState()
+
+    const searchHeader = await tenantSearch.getHeader()
+    const pageHeader = await searchHeader.getPageHeader()
+    console.log(pageHeader.getInlineActionButtons())
+    const overflowActionButton = await pageHeader.getOverflowActionMenuButton()
+    expect(overflowActionButton).toBeDefined()
+    await overflowActionButton?.click()
+    const exportItem = await pageHeader.getOverFlowMenuItem('Export all')
+    await exportItem?.selectItem()
+    expect(component.onExportItems).toHaveBeenCalled()
+  })
+  it('should dispatch search config changed action when search config changed', async () => {
+    jest.spyOn(store, 'dispatch')
+
+    component.searchConfigInfoSelectionChanged({
+      name: 'orgId',
+      displayedColumnsIds: ['orgId'],
+      fieldValues: { orgId: 'org1' },
+      viewMode: 'advanced'
+    })
+    expect(store.dispatch).toHaveBeenCalledWith(
+      TenantSearchActions.searchConfigSelected({
+        searchConfig: {
+          name: 'orgId',
+          displayedColumnsIds: ['orgId'],
+          fieldValues: { orgId: 'org1' },
+          viewMode: 'advanced'
+        }
+      })
+    )
+  })
+  it('should emit the correct diagram column', (done) => {
+    const testColumnId = 'diagram123'
+
+    const testColumn: DataTableColumn = {
+      id: testColumnId,
+      nameKey: 'diagram',
+      columnType: ColumnType.STRING
+    }
+
+    const testViewModel: TenantSearchViewModel = {
+      columns: [testColumn],
+      displayedColumns: [],
+      results: [],
+      searchCriteria: {},
+      chartVisible: false,
+      viewMode: 'advanced'
+    }
+
+    component.diagramColumnId = testColumnId
+    component.viewModel$ = of(testViewModel)
+
+    component.diagramColumn$.subscribe((result) => {
+      expect(result).toEqual(testColumn)
+      done()
+    })
+    done()
   })
 })
---
+++ src/app/tenant/pages/tenant-search/tenant-search.harness.ts
@@ -0,0 +1,14 @@
+import { ComponentHarness } from '@angular/cdk/testing'
+import {
+  GroupByCountDiagramHarness,
+  InteractiveDataViewHarness,
+  SearchHeaderHarness
+} from '@onecx/angular-accelerator/testing'
+
+export class TenantSearchHarness extends ComponentHarness {
+  static hostSelector = 'app-tenant-search'
+
+  getHeader = this.locatorFor(SearchHeaderHarness)
+  getSearchResults = this.locatorFor(InteractiveDataViewHarness)
+  getDiagram = this.locatorForOptional(GroupByCountDiagramHarness)
+}
