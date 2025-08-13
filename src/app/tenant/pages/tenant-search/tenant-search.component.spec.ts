de3e7c240f677918009d314871ff182d1f3e2eb2
Henry Taeschner <github@henry-taeschner.de>
2025-08-13 12:54:24 +0200

    fix: code cleanup

--- src/app/tenant/pages/tenant-search/tenant-search.component.spec.ts
+++ src/app/tenant/pages/tenant-search/tenant-search.component.spec.ts
@@ -1,4 +1,3 @@
-import 'zone.js'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { provideHttpClient } from '@angular/common/http'
 import { provideHttpClientTesting } from '@angular/common/http/testing'
