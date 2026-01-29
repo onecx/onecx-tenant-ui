636350adda8af37297cd5c163afb6200b9e2383d
dawpyl <203235923+dawpyl@users.noreply.github.com>
2026-01-29 15:58:50 +0100

    feat: add image preview to details compoent

--- src/app/tenant/pages/tenant-search/dialogs/tenant-create-update/tenant-create-update.types.ts
+++ src/app/tenant/pages/tenant-search/dialogs/tenant-create-update/tenant-create-update.types.ts
@@ -7,3 +7,9 @@ export interface TenantCreateUpdateViewModel {
 export type TenantCreateUpdateDialogResult = Tenant & {
   image: File | null
 }
+
+export enum TenantDialogMode {
+  DETAILS,
+  CREATE,
+  UPDATE
+}
