d1e4fb7d3c539fff0b5b04299343121b5903bb0e
Henry Taeschner <github@henry-taeschner.de>
2026-08-06 18:35:19 +0200

    fix: optimize Jest config, use babel.config.jeson

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,35 @@
+// setup-jest.ts
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone'
+
+const globalObj = globalThis as any
+const options = globalObj.ngJest?.testEnvironmentOptions || {}
+
+setupZoneTestEnv(options as any)
+
+if (globalObj.ngJest) {
+  delete globalObj.ngJest
+}
+
+/* eslint-disable @typescript-eslint/no-empty-function */
+globalThis.ResizeObserver = class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+/* fixes a bug with jsdom: ignoring this error message in log */
+const originalConsoleError = console.error
+type Err = { message: string }
+console.error = (message, ...optionalParams) => {
+  try {
+    if (message?.includes('Error: Could not parse CSS stylesheet')) return
+  } catch (err) {
+    ;(err as Err).message = `Error in console.error`
+    return
+  }
+  originalConsoleError(message, ...optionalParams)
+}
+
 Object.defineProperty(HTMLElement.prototype, 'ariaLabel', {
   get() {
     return this.getAttribute('aria-label')
@@ -12,8 +44,8 @@ globalThis.matchMedia =
   globalThis.matchMedia ||
   function () {
     return {
-      addListener: jest.fn(),
-      removeListener: jest.fn()
+      addListener: () => {},
+      removeListener: () => {}
     }
   }
 
@@ -23,29 +55,4 @@ globalThis.ngJest = {
     errorOnUnknownElements: true,
     errorOnUnknownProperties: true
   }
-}
-
-/* eslint-disable @typescript-eslint/no-empty-function */
-globalThis.ResizeObserver = class {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
-
-// setup-jest.ts
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone'
-
-setupZoneTestEnv()
-
-/* fixes a bug with jsdom: ignoring this error message in log */
-const originalConsoleError = console.error
-type Err = { message: string }
-console.error = (message, ...optionalParams) => {
-  try {
-    if (message?.includes('Error: Could not parse CSS stylesheet')) return
-  } catch (err) {
-    ;(err as Err).message = `Error in console.error`
-    return
-  }
-  originalConsoleError(message, ...optionalParams)
 }
